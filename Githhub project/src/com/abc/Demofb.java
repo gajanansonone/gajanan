86a5cbcc50ea88dd4c490c5c0e45dbe1da54adb6
Amit-Tupe <61134041+Amit-Tupe@users.noreply.github.com>
2020-02-17 12:33:06 +0530

    Update Demofb.java

--- Githhub project/src/com/abc/Demofb.java
+++ Githhub project/src/com/abc/Demofb.java
@@ -3,7 +3,7 @@ package com.abc;
 public class Demofb {
 public static void main(String[] args) {
 	
-	System.out.println("i ");
+	System.out.println("i");
 	System.out.println("am");
 	System.out.println("gaju");
 	System.out.println("sonone");
